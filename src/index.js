1b4f507d8fba10c0805b7461921b747b58238d77
hieuhoi91 <hieuhoi912002@gmail.com>
2023-02-28 20:46:40 +0700

    routes and controllers

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars  = require('express-handlebars');
+const app = express()
+const port = 3000
+
+const route = require('./routes')
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Http logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, 'resources','views'))
+
+// Routes init
+route(app)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
